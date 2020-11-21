e71ece51f95226dbc8b78d1be8823624eef5a73b
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-11-20 22:27:28 -0600

    Navbar y Formulario agregados

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar is-danger" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      </a>
+
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-end">
+        <router-link class="navbar-item" to="/">Tareas</router-link>
+        <router-link class="navbar-item" to="/About">About</router-link> 
+      </div>  
+    </div>
+  </nav>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
